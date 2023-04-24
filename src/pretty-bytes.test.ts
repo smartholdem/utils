97048a5f78db7e50cd7c54e60c4b5a0e6d5723bb
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-04-24 06:42:19 +0300

    tests

--- src/pretty-bytes.test.ts
+++ src/pretty-bytes.test.ts
@@ -6,11 +6,10 @@ describe("#prettyBytes", () => {
         expect(prettyBytes(0.4)).toBe("0.4 B");
         expect(prettyBytes(0.7)).toBe("0.7 B");
         expect(prettyBytes(10)).toBe("10 B");
-        expect(prettyBytes(10.1)).toBe("10.1 B");
+        expect(prettyBytes(10.1)).toBe("10,1 B");
         expect(prettyBytes(999)).toBe("999 B");
         expect(prettyBytes(1001)).toBe("1 kB");
-        expect(prettyBytes(1001)).toBe("1 kB");
         expect(prettyBytes(1e16)).toBe("10 PB");
-        expect(prettyBytes(1e30)).toBe("1,000,000 YB");
+        expect(prettyBytes(1e30)).toBe("1 000 000 YB");
     });
 });
